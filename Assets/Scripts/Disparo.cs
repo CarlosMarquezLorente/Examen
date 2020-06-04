e369e7afa337c506377a66949aa6943bd544c339
Carlos Márquez Lorente <c4rloswork@gmail.com>
2020-06-04 19:56:54 +0200

    2

--- Assets/Scripts/Disparo.cs
+++ Assets/Scripts/Disparo.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Disparo : MonoBehaviour
 {
     public int fuerzaDisparo = 50;
+
+    public int scorePonits;
+    public Text scoreText;
+    int score;
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            Debug.DrawRay(ray.origin, ray.direction * 100, Color.green, 5f);
+            Debug.DrawRay(ray.origin, ray.direction * 5000, Color.green, 5f);
 
             RaycastHit hit;
             bool hacolisionado = Physics.Raycast(ray, out hit);
@@ -18,7 +24,11 @@ public class Disparo : MonoBehaviour
             if (hacolisionado)
             {
                 Debug.Log(hit.collider.gameObject.name);
-                hit.collider.gameObject.GetComponent<Rigidbody>().AddForce(fuerzaDisparo * transform., ForceMode.Impulse);
+                hit.collider.gameObject.GetComponent<Rigidbody>().AddForce(fuerzaDisparo * transform.forward, ForceMode.Impulse);
+                if (hit.collider.gameObject.GetComponent<Rigidbody>().CompareTag("PickUp"))
+                {
+                    hit.collider.gameObject.gameObject.SetActive(false);
+                }
             }
         }
     }
---
+++ Assets/Scripts/NotaController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(CharacterController))]
+
+public class NotaController : MonoBehaviour
+{
+    private CharacterController characterController;
+
+    public float speed = 10f;
+    public float gravity = -9.81f;
+    public float jumpForce = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+    
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>(); 
+    }
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = 0f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 directionXZ = transform.right * x + transform.forward * z;
+        Vector3 movimento = directionXZ * speed * Time.deltaTime;
+
+        characterController.Move(movimento);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpForce * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        characterController.Move(velocity * Time.deltaTime);
+
+
+    }
+}
